e537b482fd239641fc15b3ffb52687db6ad928bf
Tanvir Ahmed <ahmedtanvir444@gmail.com>
2026-02-28 14:53:35 +0600

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      My Limited Edition Sneaker Drop App
+    </div>
+  )
+}
+
+export default App
